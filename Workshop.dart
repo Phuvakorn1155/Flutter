ad7a6184f247de9d9cecb38bcc3aba6c1660f58b
ภูวกร รุ่งปิติศุภากร <3651051541155@rmutr.ac.th>
2024-11-18 20:11:26 +0700

    Add files via upload

---
+++ Workshop.dart
@@ -0,0 +1,50 @@
+// ภูวกร รุ่งปิติศุภากร 1155
+
+//ให้สร้างแอปพลิเคชัน
+//กําหนดสีพื้นหลังให้กับหน้าจอ
+//กําหนดข้อความและสีพื้นหลังให้กับ Appbar
+//มีภาพ และคําอธิบายภาพ ใหักับ Contrainer
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My Teddy App'),
+          backgroundColor: Color(0xFFCFC3C5), // กำหนดสีพื้นหลังของ AppBar
+        ),
+        body: Container(
+          backgroundColor: Color(0xFF0E0A09), // ใช้ Hex code สีพื้นหลังหน้าจอ
+          child: Column(
+            alignment: Alignment.center,
+            children: [
+              Text(
+                'Hello Me Live Code!!!',
+                style: TextStyle(color: Color(0xFFF4ECE4), fontSize: 24),
+              ),
+              SizedBox(height: 20),
+              Image.network(             'https://static7.depositphotos.com/1119834/693/i/950/depositphotos_6936466-stock-photo-teddy-bear-with-red-rose.jpg',
+                height: 200,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'My Teddy',
+                style: TextStyle(
+                  color: Color(0xFFFEA900), // ใช้ Hex code สี FEA900
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
