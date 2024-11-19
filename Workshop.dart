1074ee07c59154cd111d7ce9b5aa9b8041a67c5f
ภูวกร รุ่งปิติศุภากร <3651051541155@rmutr.ac.th>
2024-11-19 09:55:35 +0700

    Add files via upload

--- Workshop.dart
+++ Workshop.dart
@@ -15,29 +15,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // ปิดแสดงแถบ Debug
       home: Scaffold(
         appBar: AppBar(
           title: Text('My Teddy App'),
           backgroundColor: Color(0xFFCFC3C5), // กำหนดสีพื้นหลังของ AppBar
         ),
         body: Container(
-          backgroundColor: Color(0xFF0E0A09), // ใช้ Hex code สีพื้นหลังหน้าจอ
+          color: Color(0xFF0E0A09), // ใช้ Hex code สีพื้นหลังหน้าจอ
           child: Column(
-            alignment: Alignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 'Hello Me Live Code!!!',
-                style: TextStyle(color: Color(0xFFF4ECE4), fontSize: 24),
+                style: TextStyle(
+                    color: Color(0xFFF4ECE4),
+                    fontSize: 24), //ใช้ Hex code สี Hello Me Live Code!!!
               ),
               SizedBox(height: 20),
-              Image.network(             'https://static7.depositphotos.com/1119834/693/i/950/depositphotos_6936466-stock-photo-teddy-bear-with-red-rose.jpg',
+              Image.network(
+                'https://static7.depositphotos.com/1119834/693/i/950/depositphotos_6936466-stock-photo-teddy-bear-with-red-rose.jpg',
                 height: 200,
               ),
               SizedBox(height: 10),
               Text(
                 'My Teddy',
                 style: TextStyle(
-                  color: Color(0xFFFEA900), // ใช้ Hex code สี FEA900
+                  color: Color(0xFFFEA900), // ใช้ Hex code สี My Teddy
                   fontSize: 18,
                 ),
               ),
